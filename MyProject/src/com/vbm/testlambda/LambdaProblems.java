570275ce87892092ab90957fe46431f531fc307f
vbm <vbm@localhost>
2018-08-15 07:11:49 +0530

    .

--- MyProject/src/com/vbm/testlambda/LambdaProblems.java
+++ MyProject/src/com/vbm/testlambda/LambdaProblems.java
@@ -4,7 +4,6 @@ import java.util.stream.IntStream;
 
 public class LambdaProblems {
 	public static void main(String[] args) {
-
 		//Print sum of squares of first 5 odd numbers
 		Integer result = IntStream.iterate(1,i -> i + 2)
 				 .limit(5)

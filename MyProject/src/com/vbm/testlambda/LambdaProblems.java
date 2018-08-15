6471547911591c5b1b20c5d5a415d714db58e430
vbm <vbm@localhost>
2018-08-15 07:37:27 +0530

    sum of multiples of 3 or 5 in a range

--- MyProject/src/com/vbm/testlambda/LambdaProblems.java
+++ MyProject/src/com/vbm/testlambda/LambdaProblems.java
@@ -1,13 +1,23 @@
 package com.vbm.testlambda;
 
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class LambdaProblems {
 	public static void main(String[] args) {
-		//Print sum of squares of first 5 odd numbers
+
 		Integer result = IntStream.iterate(1,i -> i + 2)
 				 .limit(5)
 				 .reduce(0,(x,y)-> x+(y*y));
 		System.out.println(result);
+
+		
+		System.out.println(IntStream.range(1, 10)
+		         .filter(n -> n%3==0 || n%5==0)
+		         .reduce(0,(x,y)-> x+y));
+		         //.forEach(System.out::println);
+		         
+		
 	}
 }

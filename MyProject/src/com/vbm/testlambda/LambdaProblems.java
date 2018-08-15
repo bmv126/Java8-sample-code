3042823c72da7c6d647163f6b198864c2a97237d
vbm <vbm@localhost>
2018-08-15 07:41:05 +0530

    sum of multiples

--- MyProject/src/com/vbm/testlambda/LambdaProblems.java
+++ MyProject/src/com/vbm/testlambda/LambdaProblems.java
@@ -7,12 +7,13 @@ import java.util.stream.IntStream;
 public class LambdaProblems {
 	public static void main(String[] args) {
 
+		//Print sum of squares of first 5 odd numbers
 		Integer result = IntStream.iterate(1,i -> i + 2)
 				 .limit(5)
 				 .reduce(0,(x,y)-> x+(y*y));
 		System.out.println(result);
 
-		
+		//Print sum of multiples of 3 or 5 in a range
 		System.out.println(IntStream.range(1, 10)
 		         .filter(n -> n%3==0 || n%5==0)
 		         .reduce(0,(x,y)-> x+y));		         
